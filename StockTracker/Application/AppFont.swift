f242ce36fb1d708700ddbf91e107c36a98951c86
Ankit Sharma <ankit.sharma10@publicissapient.com>
2024-02-02 12:17:14 +0530

    Removed Unused fonts

--- StockTracker/Application/AppFont.swift
+++ StockTracker/Application/AppFont.swift
@@ -10,16 +10,12 @@ import SwiftUI
 struct AppFont {
     
     enum RobotoFont: String {
-        case robotoBlack = "Roboto-Black"
         case robotoBold = "Roboto-Bold"
-        case robotoLight = "Roboto-Light"
-        case robotoMedium = "Roboto-Medium"
         case robotoRegular = "Roboto-Regular"
     }
     
     static let title = AppFont.font(.robotoBold, with: 24)
     static let subtitle = AppFont.font(.robotoBold, with: 16)
-    static let body = AppFont.font(.robotoMedium, with: 14)
     static let caption = AppFont.font(.robotoRegular, with: 14)
 }
 
